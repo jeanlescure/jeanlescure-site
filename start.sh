f9fd804974d26189955c17f2b7fe598f0ad121ea
Jean Lescure <jean@ticowebmedia.com>
2018-09-18 22:47:13 +0000

    Latest changes from server

---
+++ start.sh
@@ -0,0 +1,5 @@
+forever start --uid snmach -a -c "npm start --prefix /var/www/snma-ch" /var/www/snma-ch/
+forever start --uid jeanml -a -c "npm start --prefix /var/www/jean-ml" /var/www/jean-ml/
+forever start --uid sockmonkee -a -c "npm start --prefix /var/www/sockmonkee" /var/www/sockmonkee/
+forever start --uid jeanblog -a -c "npm start --production --prefix /var/www/jeanlescure/ghost" /var/www/jeanlescure/ghost/
+forever start --uid neuralsn -a -c "npm start --production --prefix /var/www/neuralsupernova" /var/www/neuralsupernova/
